3aa399b2ec79f6e8816600e41fd8fad99b86f4b2
Thomas Miller <thomasmiller@Thomass-MacBook-Air.local>
2020-06-10 12:02:08 -0400

    Created Set of Unique Values

--- app.js
+++ app.js
@@ -2,11 +2,25 @@
 
 getPlant = async () => {
   try {
-    let response = await axios.get('https://trefle.io/api/plants?page_size=7775&token=TVNmand1NnNNOUx5ZjBMcW1hbzlUUT09')
-    let plants = response.data
+    let base_url = 'https://trefle.io/api/plants?page_size=8000&token=TVNmand1NnNNOUx5ZjBMcW1hbzlUUT09'
+
+    let response = await axios.get(`${base_url}`)
     console.log(response)
+    let plantList = response.data
+    console.log(plantList)
+    let plantNames= new Set(plantList.map(seed=>{return seed.common_name}))
+    console.log(plantNames)
+    
   } catch (error) {
     console.log(`This is your error: ${error}`)
   }
 }
 getPlant()
+
+
+// (plantList[i].common_name !== 'null')
+
+// for (i = 0; i < 100; i++) {
+//   if (plantList.filter(plant)) {
+//     plantInfo = plantList[i].common_name
+//   }
